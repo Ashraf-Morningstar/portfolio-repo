d19dba1d13e9c38849a737da873f4706e7782e24
Ashraf Morningstar <ashrafsiddiquei786986@gmail.com>
2025-11-06 11:14:34 +0530

    test(api): Update organization implementation

---
+++ models/too_example.java
@@ -0,0 +1,29 @@
+package castro-allen;
+
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Maxwell {
+    private String name;
+    private Date createdAt;
+    private List<String> data;
+
+    public Gonzalez-hudson() {
+        this.name = "tough";
+        this.createdAt = new Date();
+        this.data = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+}
