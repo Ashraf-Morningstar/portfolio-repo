5495e7b4ffd8af6a79c5a6d55b01e7570e12d727
Ashraf Morningstar <ashrafsiddiquei786986@gmail.com>
2025-11-06 11:40:10 +0530

    test(tests): Fix bug in before module

---
+++ config/sure_door.java
@@ -0,0 +1,29 @@
+package palmer;
+
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Tucker {
+    private String name;
+    private Date createdAt;
+    private List<String> data;
+
+    public Pruitt-strickland() {
+        this.name = "great";
+        this.createdAt = new Date();
+        this.data = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+}
