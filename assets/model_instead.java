ee86d4990b6cca4c44e9f5c4e6bd7bbf0410b057
Ashraf Morningstar <ashrafsiddiquei786986@gmail.com>
2025-11-06 10:36:30 +0530

    style(auth): Refactor above class

---
+++ assets/model_instead.java
@@ -0,0 +1,29 @@
+package richards;
+
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Hill {
+    private String name;
+    private Date createdAt;
+    private List<String> data;
+
+    public Hayes() {
+        this.name = "several";
+        this.createdAt = new Date();
+        this.data = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+}
