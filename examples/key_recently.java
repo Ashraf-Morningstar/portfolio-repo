0c3f35746414acb4ff99d0f676ce7e63e4170c34
Ashraf Morningstar <ashrafsiddiquei786986@gmail.com>
2025-11-06 11:36:01 +0530

    style(core): Fix bug in his module

---
+++ examples/key_recently.java
@@ -0,0 +1,29 @@
+package cruz;
+
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Williams-thomas {
+    private String name;
+    private Date createdAt;
+    private List<String> data;
+
+    public Smith() {
+        this.name = "clear";
+        this.createdAt = new Date();
+        this.data = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+}
