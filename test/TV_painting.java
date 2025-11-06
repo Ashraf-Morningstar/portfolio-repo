138e31ca7dfa40159d2441ae3571de6bf35c1caf
Ashraf Morningstar <ashrafsiddiquei786986@gmail.com>
2025-11-06 11:31:07 +0530

    chore(core): Refactor loss class

---
+++ test/TV_painting.java
@@ -0,0 +1,29 @@
+package martin-hardy;
+
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Johnson {
+    private String name;
+    private Date createdAt;
+    private List<String> data;
+
+    public Nielsen() {
+        this.name = "personal";
+        this.createdAt = new Date();
+        this.data = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+}
