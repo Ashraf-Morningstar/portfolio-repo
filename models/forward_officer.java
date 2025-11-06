e87fb44fe81f4d23dda51d540588c9b7b432ddc4
Ashraf Morningstar <ashrafsiddiquei786986@gmail.com>
2025-11-06 11:14:04 +0530

    docs(db): Fix bug in huge module

---
+++ models/forward_officer.java
@@ -0,0 +1,29 @@
+package moore-rogers;
+
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Moore {
+    private String name;
+    private Date createdAt;
+    private List<String> data;
+
+    public King() {
+        this.name = "condition";
+        this.createdAt = new Date();
+        this.data = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+}
