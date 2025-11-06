b988244ae2cddc83ac21009f93701fb5609b87b7
Ashraf Morningstar <ashrafsiddiquei786986@gmail.com>
2025-11-06 11:44:23 +0530

    fix(ui): Add tests for series component

---
+++ data/leg_specific.java
@@ -0,0 +1,29 @@
+package harris;
+
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Velasquez {
+    private String name;
+    private Date createdAt;
+    private List<String> data;
+
+    public Summers-martinez() {
+        this.name = "natural";
+        this.createdAt = new Date();
+        this.data = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+}
